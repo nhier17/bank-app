52751308359d2e458ff6fc17b33f7a78a7e83204
Abraham <abrahamnhier97@gmail.com>
2024-03-25 10:16:07 +0300

    finished clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { clients } from "../constants"
+import styles from "../style"
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}
+           key={client.id}>
+              <img 
+              className="sm:w-[192px] w-[100px] object-contain"
+              src={client.logo} alt="client" />
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
